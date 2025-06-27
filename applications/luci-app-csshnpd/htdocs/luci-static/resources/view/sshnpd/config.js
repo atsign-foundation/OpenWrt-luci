df39a9c0eb31b37b4b6de5bf147b723ad9193503
Chris Swan <chris@atsign.com>
2025-06-27 14:40:39 +0100

    fix: Input validation

--- applications/luci-app-csshnpd/htdocs/luci-static/resources/view/sshnpd/config.js
+++ applications/luci-app-csshnpd/htdocs/luci-static/resources/view/sshnpd/config.js
@@ -2,26 +2,44 @@
 'require view';
 'require form';
 
+function validateAtsign(section_id, value) {
+	if (!/^@/.test(value)) {
+		return _('Must start with the "@" character.');
+	}
+	if (value.length < 2) {
+		return _('Must be at least 2 characters long (e.g., "@a").');
+	}
+	return true;
+}
+
+function validateDevice(section_id, value) {
+	if (value.length < 1) {
+		return _('Must be at least one character long (e.g., "a").');
+	}
+	if (value.length == 1) {
+		if (!/^[a-z]+$/.test(value)) {
+			return _('First character should be a lowercase letter (e.g., "a").');
+		} else {
+			return true;
+		}
+	}
+	if (!/^[a-z][a-z0-9_-]+$/.test(value)) {
+		return _('Device names may contain a-z 0-9 _ or - (e.g., "my_thing1").');
+	}
+	if (value.length > 36) {
+		return _('Maximum device length is 36 characters.');
+	}
+	return true;
+}
+
+function validateOTP(section_id, value) {
+	if (value.length != 6) {
+		return _('Must be six characters (e.g., "S3CR3T").');
+	}
+	return true;
+}
+
 return view.extend({
-	// Validator for strings starting with '@'
-	validateAtSignPrefix: function(section_id, value) {
-		let trimmedValue = (value || '').trim();
-
-		if (!/^@/.test(trimmedValue)) {
-			throw new Error(_('Must start with the "@" character.'));
-		}
-
-		if (trimmedValue.length < 2) {
-			throw new Error(_('Must be at least 2 characters long (e.g., "@a").'));
-		}
-
-		if (!/^@[a-zA-Z0-9_-]+$/.test(trimmedValue)) {
-			throw new Error(_('Can only contain letters, numbers, hyphens, and underscores after "@".'));
-		}
-
-		return true;
-	};
-
 	render: function() {
 		let m, s, o;
 
@@ -31,21 +49,28 @@ return view.extend({
 		s = m.section(form.TypedSection, 'sshnpd', _('sshnpd config'));
 		s.anonymous = true;
 
-		s.option(form.Value, 'atsign', _('Device atSign'),
+		o = s.option(form.Value, 'atsign', _('Device atSign'),
 			_('The device atSign e.g. @device'));
-		o.validate = this.validateAtSignPrefix;
+		o.default = '@device';
+		o.validate = validateAtsign;
 
-		s.option(form.Value, 'manager', _('Manager atSign'),
+		o = s.option(form.Value, 'manager', _('Manager atSign'),
 			_('The manager atSign e.g. @manager'));
+		o.default = '@manager';
+		o.validate = validateAtsign;
 
-		s.option(form.Value, 'device', _('Device name'),
+		o = s.option(form.Value, 'device', _('Device name'),
 			_('The name for this device e.g. openwrt'));
+		o.default = 'openwrt';
+		o.validate = validateDevice;
 
 		s.option(form.Value, 'args', _('Additional arguments'),
 			_('Further command line arguments for the NoPorts daemon'));
 
-		s.option(form.Value, 'otp', _('Enrollment OTP/STP'),
+		o = s.option(form.Value, 'otp', _('Enrollment OTP/SPP'),
 			_('One Time Passcode (OTP) for device atSign enrollment'));
+		o.default = '000000';
+		o.validate = validateOTP;
 
 		o = s.option(form.Flag, 'enabled', _('Enabled'),
 			_('Check here to enable the service'));
